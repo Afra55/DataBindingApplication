4d522a212d5e3604c679f8af3226a195ec608979
Afra55 <1937525930@qq.com>
2016-04-28 23:24:18 +0800

    构建

--- app/src/main/java/com/example/databinding/databindingapplication/MainActivity.java
+++ app/src/main/java/com/example/databinding/databindingapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.databinding.databindingapplication;
 
+import android.databinding.DataBindingUtil;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.databinding.databindingapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding  = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Victor", "Yang", true);
+        mBinding.setUser(user);
+        handler.sendEmptyMessageDelayed(0, 1000);
     }
+
+    private Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            User user1 = new User("Afra55", "Yang", false);
+            mBinding.setUser(user1);
+            return false;
+        }
+    });
 }
---
+++ app/src/main/java/com/example/databinding/databindingapplication/User.java
@@ -0,0 +1,27 @@
+package com.example.databinding.databindingapplication;
+
+/**
+ * Created by yangshuai in the 22:50 of 2016.04.28 .
+ */
+public class User {
+
+    private final String firstName;
+    private final String lastName;
+    private final boolean isFriends;
+
+    public User(String firstName, String lastName,boolean isFriends) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isFriends = isFriends;
+    }
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public boolean isFriends() {
+        return isFriends;
+    }
+}
